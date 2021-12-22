e1b7f8bce1ff14178e6c8eafb7387c95f041e67b
Leon <nothegger.leon@gmail.com>
2021-12-22 09:56:00 +0100

    First page

---
+++ pages/HomeForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="background box">
+    <div class="container box">
+      <h1>Github user fetching with Nuxt</h1>
+      <p v-if="errors.length" data-testid="errorDisplay" >
+        {{ errors.join(" ") }}
+      </p>
+      <!-- Submit prevent because the form is not connected, we just need the search method -->
+      <!-- <input-form @submit="search"></input-form> -->
+    </div>
+  </div>
+</template>
+
+<script>
+// import inputForm from "../components/inputForm.vue";
+export default {
+  // components: {
+  //   inputForm,
+  // },
+  data() {
+    return {
+      username: "",
+      errors: [],
+    };
+  },
+  methods: {
+    search(payload) {
+      this.username = payload;
+      if (this.username) {
+        this.$router.push({
+          name: "Results",
+          params: { username: this.username },
+        });
+      } else {
+        this.errors.push("Please enter a username.");
+      }
+    },
+  },
+};
+</script>
--- pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>New page</h1>
-</template>
-
-<script>
-export default {
-  name: 'IndexPage',
-}
-</script>
