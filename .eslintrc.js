656e0a8bbcd258ccc94f42a412af716733e8141f
Ali Eslamifard <ali.eslamifard@homeday.de>
2022-01-31 13:12:07 +0100

    Update linter and required packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
     browser: true,
     node: true,
   },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false,
-  },
+  // parserOptions: {
+  //   parser: '@babel/eslint-parser',
+  //   requireConfigFile: false,
+  // },
   extends: [ '@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
