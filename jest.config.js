357a96ea5db3b3ab8bbadd0f99e550122a137154
Leon <nothegger.leon@gmail.com>
2022-02-01 18:51:21 +0100

    Setup testing library and added first tests

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,16 @@ module.exports = {
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js',
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'vue',
+    'json'
+  ],
   transform: {
+    "^.+\\.ts$": "ts-jest",
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    '.*\\.(vue)$': 'vue-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
